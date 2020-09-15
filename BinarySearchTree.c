7fbd2d647a13448b691b3fe8e8e5f11f28916432
mobaier <70043923+mobaier@users.noreply.github.com>
2020-09-14 22:11:44 -0300

    Added item remover function

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -51,25 +51,81 @@ struct node* inserir(struct node* no, int chave)
   
     return no; 
 } 
-   
-// No main é requisitado ao usuário que insira 
-// "n" valores na árvore. Em seguida a árvore é exibida
-// ordenadamente
-int main() 
+
+// Dado uma ABB não vazia, retornar o nó com a menor chave daqula subárvore  
+struct node * menorValorNo(struct node* node) 
 { 
-    int entrada;
-    int valor;
+    struct node* atual = node; 
+  
+    // Percorrer a árvore até o chegar no valor mais a esquerda da ABB
+    while (atual && atual->esquerda != NULL) 
+        atual = atual->esquerda; 
+  
+    return atual; 
+} 
 
+// Remover nó da árvore
+struct node* remover(struct node* raiz, int chave) 
+{ 
+    // Árvore sem itens 
+    if (raiz == NULL) return raiz; 
+  
+    // Se a chave a ser deletada for menor que a raiz, 
+    // então ela fica na árvore da direita. 
+    if (chave < raiz->chave) 
+        raiz->esquerda = remover(raiz->esquerda, chave); 
+  
+    // Se a chave a ser deletada for maior que a raiz, 
+    // então ela fica na árvore da esquerda.
+    else if (chave > raiz->chave) 
+        raiz->direita = remover(raiz->direita, chave); 
+  
+    // Se a chave for igual o nó é removido
+    else
+    { 
+        // Nó com 1 ou menos subárvores
+        if (raiz->esquerda == NULL) 
+        { 
+        	struct node *temp = raiz->direita;
+            free(raiz); 
+            return temp; 
+        } 
+        else if (raiz->direita == NULL) 
+        { 
+            struct node *temp = raiz->esquerda; 
+            free(raiz); 
+            return temp; 
+        } 
+  
+        // Nó com 2 subárvores: Pegar o sucessor pelo ordem (menor da árvore da direita) 
+        struct node* temp = menorValorNo(raiz->direita); 
+
+        // Passar o conteúdo do sucessor para este nó
+        raiz->chave = temp->chave; 
+  
+        // Remover o sucessor 
+        raiz->direita = remover(raiz->direita, temp->chave); 
+    } 
+    return raiz; 
+} 
+   
+/* --------------------------------------------------------------
+LISTA DE FUNÇÕES:
+- struct node *novoNo(int item)
+- void exibir(struct node *raiz)
+- struct node* inserir(struct node* no, int chave)
+- struct node * menorValorNo(struct node* node)
+- struct node* remover(struct node* raiz, int chave) 	
+--------------------------------------------------------------*/	
+int main(void) 
+{ 
     struct node *arvore = NULL;
-    do {
-      printf("Insira um valor: ");
-      scanf("%i", &valor);
 
-      arvore = inserir(arvore, valor);
-      printf("Deseja inserir outro valor? (0 = Negativo)");
-      scanf("%i", &entrada);
-    }while(entrada != 0);
-
+    arvore = inserir(arvore, 10);
+    arvore = inserir(arvore, 2);
+    arvore = inserir(arvore, 6);
+    arvore = inserir(arvore, 8);
+    arvore = remover(arvore, 6);
     exibir(arvore);
     
     system("PAUSE");
